2294d0c23f456483bbe2242169383fbc3254142b
Delgadoc86 <delgadoc1986@hotmail.com>
2023-07-21 20:41:10 -0300

    codigo modificado 2

---
+++ punto 3/src/BucleWhile.java
@@ -0,0 +1,17 @@
+/*Para el bucle Do While, deberás crear la misma estructura
+que en el While, pero solo se debe ejecutar una vez.*/
+
+
+
+public class BucleWhile{
+
+
+    public static void main(String[] args) {
+        int numeroDoWhile = 0;
+
+        do {
+            numeroDoWhile++; // Incrementa el valor de la variable en uno cada vez que se ejecuta
+            System.out.println("Valor actual de numeroDoWhile: " + numeroDoWhile); // Muestra el valor por pantalla
+        } while (numeroDoWhile < 3);
+    }
+}
